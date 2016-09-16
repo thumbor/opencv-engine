Fixed rotation matrix calculation
Fixed constant syntax for BGRA2GRAY

Fixed array initialization for grayscale images as BGR

Removed default thumbor install requirement

opencv_engine/engine_cv3.py
@@ -30,14 +30,18 @@ class Engine(BaseEngine):
     @property
     def image_depth(self):
         if self.image is None:
-            return 8
-        return self.image.dtype.itemsize
+            return np.uint8
+        return self.image.dtype
 
     @property
     def image_channels(self):
         if self.image is None:
             return 3
-        return self.image.shape[2]
+        # if the image is grayscale
+        try:
+            return self.image.shape[2]
+        except IndexError:
+            return 1
 
     @classmethod
     def parse_hex_color(cls, color):
@@ -48,14 +52,17 @@ class Engine(BaseEngine):
             return None
 
     def gen_image(self, size, color_value):
-        img = np.zeros((size[1], size[0]), self.image.dtype, self.image_channels)
         if color_value == 'transparent':
             color = (255, 255, 255, 255)
+            img = np.zeros((size[1], size[0], 4), self.image_depth)
         else:
+            img = np.zeros((size[1], size[0], self.image_channels), self.image_depth)
             color = self.parse_hex_color(color_value)
             if not color:
                 raise ValueError('Color %s is not valid.' % color_value)
+        print(color)
         img[:] = color
+        print('DONE')
         return img
 
     def create_image(self, buffer):
@@ -98,9 +105,9 @@ class Engine(BaseEngine):
 
     def rotate(self, degrees):
         shape = self.image.shape
-        image_center = tuple(np.array(shape[1], shape[0]) / 2)
+        image_center = (shape[0] / 2, shape[1] / 2)
         rot_mat = cv2.getRotationMatrix2D(image_center, degrees, 1.0)
-        self.image = cv2.warpAffine(self.image, rot_mat, self.image.shape, flags=cv2.INTER_LINEAR)
+        self.image = cv2.warpAffine(self.image, rot_mat, dsize=shape[0:2])
 
     def flip_vertically(self):
         self.image = np.flipud(self.image)
@@ -149,7 +156,7 @@ class Engine(BaseEngine):
         else:
             mode = 'BGR'
             shape = self.image.shape
-            rgb_copy = np.zeros((shape[1], shape[0]), np.uint8, 3)
+            rgb_copy = np.zeros((shape[1], shape[0], 3), self.image.dtype)
             cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR, rgb_copy)
             self.image = rgb_copy
         return mode, self.image.tostring()
@@ -160,7 +167,7 @@ class Engine(BaseEngine):
     def convert_to_grayscale(self):
         if self.image_channels >= 3:
             shape = self.image.shape
-            self.image = cv2.cvtColor(self.image, cv2COLOR_BGRA2GRAY)
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGRA2GRAY)
 
     def paste(self, other_engine, pos, merge=True):
         if merge and not FILTERS_AVAILABLE:
@@ -187,9 +194,9 @@ class Engine(BaseEngine):
         if self.image_channels < 4:
             shape = self.image.shape
             print(type(shape[1]), type(shape[0]), type(self.image.dtype))
-            with_alpha = np.zeros((shape[1], shape[0]), self.image.dtype, 4)
+            with_alpha = np.zeros((shape[1], shape[0], 4), self.image.dtype)
             if self.image_channels == 3:
                 cv2.cvtColor(self.image, cv2.COLOR_BGR2BGRA, with_alpha)
             else:
-                cv2.CvtColor(self.image, cv2.COLOR_GRAY2BGRA, with_alpha)
+                cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGRA, with_alpha)
             self.image = with_alpha